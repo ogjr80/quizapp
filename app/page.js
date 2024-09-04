240ca544bd37f62f823ecd8fbb623dcc9ba3040e
Gideon Ogongo <ogjr80@gmail.com>
2024-09-04 18:05:33 +0200

    added a bit of touch

--- app/page.js
+++ app/page.js
@@ -2,14 +2,16 @@
 // import GameBoard from "./components/GameBoard";
 import Quizapp from "./components/Quizapp";
 import QuizGame from "./components/Quizgame";
+import HeritageCardGame from "./components/HeritageGame";
 // import HeritageCardGame from "./components/Shuffle";
-import HeritageCardGame from "./components/Shuffle2";
+// import HeritageCardGame from "./components/Shuffle2";
 // import GameBoard from "./components/GameboardWithLeaderboard";
 // import Confetti from "react-confetti/dist/types/Confetti";
 export default function Home() {
   return (
     <div> 
       {/* <Quizapp />  */}
+      {/* <HeritageCardGame />  */}
       <HeritageCardGame /> 
     </div>
   )
