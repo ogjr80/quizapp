5852fa68ca4464fd74a80883d5b0b549d5f2448d
Gideon Ogongo <ogjr80@gmail.com>
2024-09-04 17:37:36 +0200

    added multiple cards

--- app/page.js
+++ app/page.js
@@ -2,12 +2,15 @@
 // import GameBoard from "./components/GameBoard";
 import Quizapp from "./components/Quizapp";
 import QuizGame from "./components/Quizgame";
+// import HeritageCardGame from "./components/Shuffle";
+import HeritageCardGame from "./components/Shuffle2";
 // import GameBoard from "./components/GameboardWithLeaderboard";
 // import Confetti from "react-confetti/dist/types/Confetti";
 export default function Home() {
   return (
     <div> 
-      <Quizapp /> 
+      {/* <Quizapp />  */}
+      <HeritageCardGame /> 
     </div>
   )
 }
