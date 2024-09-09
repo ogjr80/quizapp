90bbf443a2801ca69fc415d21a06b5745c88bdbc
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-09 15:32:38 +0200

    create 1 file and update 1 file

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+  serverRuntimeConfig: {
+    VERCEL_FUNCTIONS_DISABLE: true,
+  },
+
   images: {
     remotePatterns: [
       {
