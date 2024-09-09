3e3dbc5f766870d8a46da42045df4e56b09e02f3
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-09 15:52:53 +0200

    fix: add a trailing space to the description in metadata for layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,7 +6,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Heritage Day Quest",
-  description: "Heritage Day Quest - Quiz App",
+  description: "Heritage Day Quest - Quiz App ",
 };
 
 export default function RootLayout({ children }) {
