64f549c8ab2efd348709e6d30fe0597787969a9c
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-18 16:25:24 +0200

    update dev.yml, page.js, auth.js and utils.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,16 @@
-'use client';
 import { exempted } from "@/lib/auth";
 import HeritageCardGame from "./components/HeritageLatest";
 import { CountdownComponent } from "@opherlabs/components";
 export default function Home() {
   return (
     <div>
+     <pre>
+       {JSON.stringify(process.env, null, 2)}
+     </pre>
       {
         !exempted.includes(process.env.AUTH_URL) ? <HeritageCardGame />
           : <CountdownComponent targetDate={'2024-09-26T12:00:00'} />
       }
-      <CountdownComponent targetDate={'2024-09-26T12:00:00'} />
     </div>
   )
 }
