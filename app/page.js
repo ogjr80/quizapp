0ea3db861ac67720a505f87fe96df00531e071f8
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-18 16:30:18 +0200

    update page.js

--- app/page.js
+++ app/page.js
@@ -4,9 +4,6 @@ import { CountdownComponent } from "@opherlabs/components";
 export default function Home() {
   return (
     <div>
-     <pre>
-       {JSON.stringify(process.env, null, 2)}
-     </pre>
       {
         !exempted.includes(process.env.AUTH_URL) ? <HeritageCardGame />
           : <CountdownComponent targetDate={'2024-09-26T12:00:00'} />
