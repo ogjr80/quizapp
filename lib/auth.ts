2cb8be87dde6220444e51c8a00686dd8b6cfcbcd
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 05:55:45 +0200

    implement authentication using NextAuth with Google and Microsoft Entra in auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,27 @@
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./prisma";
+import Entra from "next-auth/providers/microsoft-entra-id";
+import { isProd } from "./utils";
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  theme:{
+    colorScheme: 'light',
+    logo: '/eoh.png',
+  },
+  debug: process.env.NODE_ENV === "development",
+  providers: [
+    !isProd ? Google :
+    Entra({
+      clientId: process.env.AUTH_AZURE_AD_ID,
+      clientSecret: process.env.AUTH_AZURE_AD_SECRET,
+      tenantId: process.env.AUTH_AZURE_AD_TENANT,
+    }),
+    
+  ],
+  adapter: PrismaAdapter(prisma),
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.AUTH_SECRET,
+});
