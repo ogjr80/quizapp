1ad5b48562b57028fa8146e1cacc817ef7da8b93
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 08:23:40 +0200

    update page.tsx and quizdata.ts update quizdata.ts

--- app/universe/page.tsx
+++ app/universe/page.tsx
@@ -1,10 +1,17 @@
 'use client';
-import { CountdownComponent } from "@opherlabs/components";
+import { CardsHolder, CountdownComponent, QuizCards } from "@opherlabs/components";
 import React from "react";
+import { files } from "./quizdata";
 export default function Home() {
+  const target = new Date("September 26, 2024 20:00:00").getTime();
+  const now = new Date().getTime();
+  const timeLeft = target - now;
   return (
     <div>
-          { CountdownComponent({ url: '/api/auth/signin', targetDate: '2024-09-26T20:00:00' })}
+          { timeLeft<=0?<>
+            <CardsHolder>
+                <QuizCards url="universe" files={files} />
+            </CardsHolder></> : CountdownComponent({ url: '/api/auth/signin', targetDate: '2024-09-26T20:00:00' })}
     </div>
   )
 }
