016771f5d335b8ca43c1e0714eb0748bb06eaebb
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 08:15:32 +0200

    refactor(page.tsx): remove CountdownComponent import and update QuizCards URL to 'public'

--- app/public/page.tsx
+++ app/public/page.tsx
@@ -1,16 +1,12 @@
 'use client';
-import { QuizCards, CardsHolder, CountdownComponent } from "@opherlabs/components";
+import { QuizCards, CardsHolder } from "@opherlabs/components";
 import React from "react";
 import { files } from "../universe/quizdata";
-import { exemptedList } from "@/lib/utils";
 export default function Home() {
-    // const isProd = exemptedList.includes(process.env.AUTH_URL ?? process.env.NEXT_PUBLIC_URL)
-
-   
     return (
         <div>
             <CardsHolder>
-                <QuizCards url="universe" files={files} />
+                <QuizCards url="public" files={files} />
             </CardsHolder>
 
         </div>
