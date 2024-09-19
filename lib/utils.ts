22b73e28d81ed60491a66cbd962ee56f6e373009
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 07:13:24 +0200

    update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,5 +5,5 @@ export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
 
-const exemptedList = ["https://unityindiversity.co.za", "https://www.unityindiversity.co.za"];
+export const exemptedList = ["https://unityindiversity.co.za","http://localhost:3000", "https://www.unityindiversity.co.za"];
 export const isProd=exemptedList.includes(process.env.AUTH_URL?? process.env.NEXT_PUBLIC_URL)
