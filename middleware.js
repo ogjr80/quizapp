cdcda5f26fc24740f2e9e490d59089bd48524919
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-19 07:28:05 +0200

    update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@ import { auth } from "./lib/auth";
 
 export default auth((req) => {
   if (!req.auth && protectedRoutes.includes(req.nextUrl.pathname)) {
-    const url = req.url.replace(req.nextUrl.pathname, "/public");
+    const url = req.url.replace(req.nextUrl.pathname, "/api/auth/signin");
     return Response.redirect(url);
   }
 });
