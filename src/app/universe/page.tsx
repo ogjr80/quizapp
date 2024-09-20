04896fbd5120b69525bd916a018f11f4235cf2a6
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-20 17:11:09 +0200

    create 3 files and update 3 files

--- src/app/universe/page.tsx
+++ src/app/universe/page.tsx
@@ -3,7 +3,9 @@ import type { NextPage } from 'next';
 import { CountdownComponent } from "@/components"
 
 import { GamePlay } from "@/components"
+import { useSession } from 'next-auth/react';
 const Home: NextPage = () => {
+    const {data:session} = useSession()
     const target = new Date("September 20, 2024 20:00:00").getTime();
     const now = new Date().getTime();
     const timeLeft = target - now;
@@ -11,7 +13,7 @@ const Home: NextPage = () => {
         <div className="bsg">
             {timeLeft <= 0 ? <>
                 <GamePlay />
-            </> : CountdownComponent({ url: '/api/auth/signin', targetDate: '2024-09-20T20:00:00' })}
+            </> : CountdownComponent({ session, url: '/api/auth/signin', targetDate: '2024-09-20T20:00:00' })}
         </div>
     );
 };
