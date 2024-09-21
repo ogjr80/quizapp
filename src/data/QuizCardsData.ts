551538f5519b6eaf74fa48fe37c19447a3a8f915
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-21 12:45:09 +0200

    create 5 files and update 1 file

--- src/data/QuizCardsData.ts
+++ src/data/QuizCardsData.ts
@@ -13,10 +13,204 @@ export const QuizCardsData = [
                 timer: 30
             },
             {
-                question: "Which language is the most spoken in South Africa?",
-                options: ["Zulu", "Xhosa", "Afrikaans", "English"],
-                correctAnswer: "Zulu",
-                timer: 30
+                "id": "dq3",
+                timer: 30,
+                "question": "What is the national flower of South Africa?",
+                "options": ["Protea", "Daisy", "Sunflower", "Rose"],
+                "correctAnswer": "Protea",
+                "explanation": "The King Protea is the national flower of South Africa. It symbolizes beauty, resilience, and strength, and is often associated with the country's natural diversity and unity."
+            },
+            {
+                "id": "dq4",
+                timer: 30,
+                "question": "Which of the following is NOT one of South Africa's 11 official languages?",
+                "options": ["Zulu", "Xhosa", "Swahili", "Afrikaans"],
+                "correctAnswer": "Swahili",
+                "explanation": "South Africa has 11 official languages, including Zulu, Xhosa, Afrikaans, English, and Sesotho, but Swahili is not one of them."
+            },
+            {
+                "id": "dq5",
+                timer: 30,
+                "question": "Which South African cultural group is known for their intricate beadwork and colorful attire, especially during ceremonies?",
+                "options": ["Zulu", "Xhosa", "Sotho", "Venda"],
+                "correctAnswer": "Zulu",
+                "explanation": "The Zulu cultural group is known for their elaborate beadwork and vibrant ceremonial attire."
+            },
+            {
+                "id": "dq6",
+                timer: 30,
+                "question": "What is the traditional dish called that is commonly enjoyed by people of Indian descent in Durban, often served in a hollowed-out loaf of bread?",
+                "options": ["Bobotie", "Bunny Chow", "Braai", "Biltong"],
+                "correctAnswer": "Bunny Chow",
+                "explanation": "Bunny Chow is a dish consisting of a hollowed-out loaf of bread filled with curry, popular among the Indian community in Durban."
+            },
+            {
+                "id": "dq7",
+                timer: 30,
+                "question": "What is the name of the iconic mountain that overlooks Cape Town and is considered one of the New7Wonders of Nature?",
+                "options": ["Table Mountain", "Lion's Head", "Devil's Peak", "Signal Hill"],
+                "correctAnswer": "Table Mountain",
+                "explanation": "Table Mountain is the iconic flat-topped mountain overlooking Cape Town and is recognized as one of the New7Wonders of Nature."
+            },
+            {
+                "id": "dq8",
+                timer: 30,
+                "question": "Which famous South African landmark served as a prison for political leaders, including Nelson Mandela, during the apartheid era?",
+                "options": ["Alcatraz", "Robben Island", "Devil's Island", "Pollsmoor Prison"],
+                "correctAnswer": "Robben Island",
+                "explanation": "Robben Island, located off the coast of Cape Town, served as a prison for political leaders, including Nelson Mandela, during the apartheid era."
+            },
+            {
+                "id": "dq9",
+                timer: 30,
+                "question": "What is the name of the vast national park in South Africa known for its rich wildlife and is one of the largest game reserves in Africa?",
+                "options": ["Addo Elephant Park", "Kruger National Park", "Pilanesberg National Park", "iSimangaliso Wetland Park"],
+                "correctAnswer": "Kruger National Park",
+                "explanation": "Kruger National Park is one of the largest game reserves in Africa, known for its diverse wildlife and vast size."
+            },
+            {
+                "id": "dq10",
+                timer: 30,
+                "question": "Which South African ethnic group primarily speaks Sesotho?",
+                "options": ["Zulu", "Xhosa", "Basotho", "Tswana"],
+                "correctAnswer": "Basotho",
+                "explanation": "The Basotho people primarily speak Sesotho, and they are predominantly found in the Free State province and Lesotho."
+            },
+            {
+                "id": "dq11",
+                timer: 30,
+                "question": "Who coined the term 'Rainbow Nation' to describe South Africa's racial and cultural diversity?",
+                "options": ["Nelson Mandela", "Desmond Tutu", "F.W. de Klerk", "Thabo Mbeki"],
+                "correctAnswer": "Desmond Tutu",
+                "explanation": "The term 'Rainbow Nation' was coined by Archbishop Desmond Tutu to describe South Africa's peaceful transition from apartheid to a democracy that embraces all races and cultures."
+            },
+            {
+                "id": "dq12",
+                timer: 30,
+                "question": "Which South African cultural group is known for preparing bobotie, a dish made of spiced minced meat baked with an egg-based topping?",
+                "options": ["Zulu", "Xhosa", "Cape Malay", "Afrikaner"],
+                "correctAnswer": "Cape Malay",
+                "explanation": "The Cape Malay community is known for preparing bobotie, a dish made of spiced minced meat baked with an egg-based topping."
+            },
+            {
+                "id": "dq13",
+                timer: 30,
+                "question": "What does the 'i' in iOCO stand for?",
+                "options": ["Internet", "Information", "Innovative", "Inspired"],
+                "correctAnswer": "Inspired",
+                "explanation": "The 'i' in iOCO stands for 'Inspired', as mentioned in the company description: 'Inspired by digitally native internet organisations (iO)'."
+            },
+            {
+                "id": "dq14",
+                timer: 30,
+                "question": "What does the 'CO' in iOCO represent?",
+                "options": ["Company Operations", "Creative Organisations", "Corporate Objectives", "Customer Oriented"],
+                "correctAnswer": "Creative Organisations",
+                "explanation": "The 'CO' in iOCO represents 'Creative Organisations', as stated in the company description: 'and creative organisations (CO) of the future'."
+            },
+            {
+                "id": "dq15",
+                timer: 30,
+                "question": "What is the essence of the iOCO brand?",
+                "options": ["Technological innovation", "Community and technology serving humanity", "Corporate success", "Global expansion"],
+                "correctAnswer": "Community and technology serving humanity",
+                "explanation": "The essence of the iOCO brand is described as 'the coming together of a community of great people with the best technology to serve humanity'."
+            },
+            {
+                "id": "dq16",
+                timer: 30,
+                "question": "What is iOCO's purpose?",
+                "options": ["Maximizing profits", "Empowering business and society", "Developing new technologies", "Expanding globally"],
+                "correctAnswer": "Empowering business and society",
+                "explanation": "iOCO's purpose is stated as 'Empowering business and society through technology and people'."
+            },
+            {
+                "id": "dq17",
+                timer: 30,
+                "question": "How does iOCO describe itself in terms of its role in the ICT industry?",
+                "options": ["Software developer", "Hardware manufacturer", "Systems integrator", "Telecommunications provider"],
+                "correctAnswer": "Systems integrator",
+                "explanation": "iOCO describes itself as a systems integrator: 'As a systems integrator, our strategy as iOCO will be driven by the integrated strength of our business units'."
+            },
+            {
+                "id": "dq18",
+                timer: 30,
+                "question": "What is the primary focus of iOCO's strategy?",
+                "options": ["Individual business unit success", "Integrated strength of business units", "Market domination", "Cost reduction"],
+                "correctAnswer": "Integrated strength of business units",
+                "explanation": "iOCO's strategy is 'driven by the integrated strength of our business units, for the benefit of our clients and partners'."
+            },
+            {
+                "id": "dq19",
+                timer: 30,
+                "question": "Which larger group is iOCO a part of?",
+                "options": ["IBM Group", "Microsoft Group", "EOH Group", "Oracle Group"],
+                "correctAnswer": "EOH Group",
+                "explanation": "The content states that 'iOCO is the ICT services brand of the EOH Group'."
+            },
+            {
+                "id": "dq20",
+                timer: 30,
+                "question": "What is iOCO's email domain for contact?",
+                "options": ["@ioco.com", "@ioco.tech", "@eoh.com", "@ioco.co.za"],
+                "correctAnswer": "@ioco.tech",
+                "explanation": "The contact email provided is 'solve@ioco.tech'."
+            },
+            {
+                "id": "dq21",
+                timer: 30,
+                "question": "In which area is iOCO's business park located?",
+                "options": ["Johannesburg", "Cape Town", "Durban", "Sandton"],
+                "correctAnswer": "Sandton",
+                "explanation": "The address provided for the EOH Business Park is in Sandown, Sandton."
+            },
+            {
+                "id": "dq22",
+                timer: 30,
+                "question": "What does iOCO aim to navigate for its clients?",
+                "options": ["Financial markets", "Technological challenges", "Path to an exponential future", "Global expansion"],
+                "correctAnswer": "Path to an exponential future",
+                "explanation": "iOCO invites clients to 'Join us as we navigate your path to an exponential future'."
+            },
+            {
+                "id": "dq23",
+                timer: 30,
+                "question": "What does EOH's purpose 'SOLVE' stand for?",
+                "options": ["Service Oriented Leadership Values Excellence", "Sustainable Operations Leveraging Valuable Expertise", "Strategic Optimization of Lucrative Ventures and Enterprises", "Solutions Oriented Leadership Values Excellence"],
+                "correctAnswer": "Solutions Oriented Leadership Values Excellence",
+                "explanation": "EOH's purpose is to SOLVE, which likely stands for Solutions Oriented Leadership Values Excellence, aligning with their focus on providing technology solutions and services."
+            },
+            {
+                "id": "dq24",
+                timer: 30,
+                "question": "How many clients does EOH's core ICT business serve?",
+                "options": ["Around 1,000", "Approximately 3,000", "About 5,000", "Over 10,000"],
+                "correctAnswer": "About 5,000",
+                "explanation": "The Group's core ICT business, operating under the iOCO brand, serves a diversified client base of approximately 5,000 clients."
+            },
+            {
+                "id": "dq25",
+                timer: 30,
+                "question": "What is EOH's B-BBEE (Broad-based Black Economic Empowerment) contributor level?",
+                "options": ["Level 1", "Level 2", "Level 3", "Level 4"],
+                "correctAnswer": "Level 1",
+                "explanation": "As a proudly South African business, EOH is committed to sustainable transformation and is a Level 1 Broad-based Black Economic Empowerment (B-BBEE) contributor."
+            },
+            {
+                "id": "dq26",
+                timer: 30,
+                "question": "How many employees does EOH have?",
+                "options": ["About 3,000", "Approximately 4,500", "Around 5,986", "Over 7,000"],
+                "correctAnswer": "Around 5,986",
+                "explanation": "The Group's 5,986 employees are intrinsic to its ability to deliver world-class services to customers across all major industries throughout South Africa."
+            },
+            {
+                "id": "dq27",
+                timer: 30,
+                "question": "Which of the following is NOT mentioned as one of EOH's digital transformation capabilities?",
+                "options": ["Managed Services", "Digital Infrastructure", "Blockchain Solutions", "Cloud Solutions"],
+                "correctAnswer": "Blockchain Solutions",
+                "explanation": "EOH's digital transformation capabilities include Managed Services, Digital Infrastructure, Security, Automation, Cloud Solutions, Data, and App Dev, but Blockchain Solutions is not specifically mentioned."
             }
         ]
     },
