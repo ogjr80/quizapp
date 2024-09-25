1b14d093ef7ccea48d7d10c98459551731d72a8b
Mitch Chanza <mitch.chanza@gmail.com>
2024-09-25 21:33:16 +0200

    update useSessionCounter.tsx

--- src/hooks/useSessionCounter.tsx
+++ src/hooks/useSessionCounter.tsx
@@ -7,30 +7,29 @@ export const useSessionCounter = () => {
     const [isTimeUp, setIsTimeUp] = useState(session?.isActive)
     const [progress, setProgress] = useState(100)
     useEffect(() => {
-        if (session && session.isActive && session.endTime) {
-            const totalDuration = new Date(session.endTime).getTime() - new Date(session.startTime).getTime()
-
+        if (session?.isActive && session?.endTime) {
+            const totalDuration = new Date(session.endTime).getTime() - new Date(session.startTime).getTime();
+            
             const timer = setInterval(() => {
-                const now = new Date().getTime()
-                const end = new Date(session.endTime).getTime()
-                const difference = end - now
+                const now = new Date().getTime();
+                const end = new Date(session.endTime).getTime();
+                const difference = end - now;
 
                 if (difference > 0) {
-                    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))
-                    const seconds = Math.floor((difference % (1000 * 60)) / 1000)
-                    setTimeLeft(`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`)
-                    setProgress((difference / totalDuration) * 100)
+                    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+                    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+                    setTimeLeft(`${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`);
+                    setProgress((difference / totalDuration) * 100);
                 } else {
-                    setTimeLeft('00:00')
-                    // TODO: send event to server to end the game
-                    endGame.mutate()
-                    setIsTimeUp(session?.isActive)
-                    setProgress(0)
-                    clearInterval(timer)
+                    setTimeLeft('00:00');
+                    endGame.mutate();
+                    setIsTimeUp(session?.isActive);
+                    setProgress(0);
+                    clearInterval(timer);
                 }
-            }, 1000)
+            }, 1000);
 
-            return () => clearInterval(timer)
+            return () => clearInterval(timer);
         }
     }, [session, endGame])
     return {
